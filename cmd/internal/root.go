d858e676be084da26a6d522ae2a226a9cf467a5d
evias <141805+evias@users.noreply.github.com>
2026-09-21 18:10:29 +0200

    fix: removes unnecessary signal traps

--- cmd/internal/root.go
+++ cmd/internal/root.go
@@ -1,12 +1,8 @@
 package internal
 
 import (
-	"fmt"
 	"log/slog"
-	"os"
-	"os/signal"
 	"path/filepath"
-	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -34,34 +30,4 @@ func init() {
 
 func Cmd() *cobra.Command {
 	return rootCmd
-}
-
-// Internal interface for logging
-type logger interface {
-	Info(msg string, keyvals ...interface{})
-}
-
-// TrapSignal catches the SIGTERM/SIGINT and executes cb function, then it exits
-// with code 0.
-func TrapSignal(logger logger, cb func(), sigs ...os.Signal) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, sigs...)
-	go func() {
-		for sig := range c {
-			logger.Info("signal trapped", "msg", fmt.Sprintf("captured %v, exiting...", sig))
-			if cb != nil {
-				cb()
-			}
-			os.Exit(0)
-		}
-	}()
-}
-
-// Kill the running process by sending itself SIGTERM.
-func Kill() error {
-	p, err := os.FindProcess(os.Getpid())
-	if err != nil {
-		return err
-	}
-	return p.Signal(syscall.SIGTERM)
 }
